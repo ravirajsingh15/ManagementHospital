631eb85ca599ab8e5b944085779f93f79e8f6855
Ravirajsingh <raviraj.singh@reddoorz.comgit config --global user.email raviraj.singh@reddoorz.com>
2023-01-17 11:29:01 +0530

    first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	validates :name, :last_name, :address, :mobile_no, :email, presence: true
-	validates :name, presence: true
+	validates :name, presence: true, uniqueness: true
 	validates :last_name, presence: true, uniqueness: true
 	validates :address, presence: true, uniqueness: true
 	validates :mobile_no, presence: true, uniqueness: true, length: { maximum: 10 }
